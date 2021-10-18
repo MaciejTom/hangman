e9c16f55f1e4ca6aca549a5e8e3f969e9eaabe6a
Maciej Tomaszewicz <1994tomaszewicz@gmail.com>
2021-10-18 17:18:00 +0200

    basic filed added

---
+++ app/Game.js
@@ -0,0 +1,15 @@
+class Game  {
+    constructor({lettersWrapper, categoryWrapper, wordWrapper, outputWrapper}) {
+        
+        this.lettersWrapper = lettersWrapper
+        this.categoryWrapper = categoryWrapper
+        this.wordWrapper = wordWrapper
+        this.outputWrapper = outputWrapper
+        
+    } 
+
+    start() {
+        
+    }
+}
+
